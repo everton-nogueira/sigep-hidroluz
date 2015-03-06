a26c29acb7c58e652333d0c5ae6b8e84797658d0
Evertonnp10@gmail.com <Evertonnp10@gmail.com@cb3fc497-91f2-51d1-55bd-55d06f6778d8>
2015-03-06 03:03:14 +0000

    Initial import.

---
+++ trunk/sigep/sigep-web/src/main/java/br/com/hidroluz/mb/PocoMBean.java
@@ -0,0 +1,16 @@
+/**
+ * Desenvolvido por Everton e Willian 23/02/2015
+ */
+package br.com.hidroluz.mb;
+
+import javax.faces.bean.ManagedBean;
+
+/**
+ * @author Everton
+ * Data: 23/02/2015
+ */
+@ManagedBean
+public class PocoMBean {
+	
+	
+}
