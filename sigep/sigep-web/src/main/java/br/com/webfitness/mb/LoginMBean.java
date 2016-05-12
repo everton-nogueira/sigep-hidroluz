b71940ffc89a3218deb6f971a65f92b9b21cdcf0
everton-nogueira <evertonnp10@gmail.com>
2016-05-11 23:58:15 -0300

    Ajustes na página de Login

--- sigep/sigep-web/src/main/java/br/com/webfitness/mb/LoginMBean.java
+++ sigep/sigep-web/src/main/java/br/com/webfitness/mb/LoginMBean.java
@@ -3,10 +3,24 @@
  */
 package br.com.webfitness.mb;
 
+import javax.faces.bean.ManagedBean;
+
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * @author Everton
  * Data: 08/05/2016
  */
+@ManagedBean
 public class LoginMBean {
 
+	@Getter @Setter
+	private String login;
+	@Getter @Setter
+	private String senha;
+	
+	public void logar() {
+		
+	}
 }
