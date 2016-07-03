6b08fbebb5ff3ad3e6c6bec69d269994ceb45381
everton-nogueira <evertonnp10@gmail.com>
2016-07-03 14:12:20 -0300

    Correção do FONT-AWESOME... Só falta apontar pra imagem dentro do resource pro template ficar pronto!

--- sigep/sigep-web/src/main/java/br/com/webfitness/jsf/listener/SessionListener.java
+++ sigep/sigep-web/src/main/java/br/com/webfitness/jsf/listener/SessionListener.java
@@ -5,9 +5,14 @@ package br.com.webfitness.jsf.listener;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
+
+import br.com.webfitness.DTO.PessoaDTO;
+import br.com.webfitness.constante.ConstantesWebFitness;
 
 /**
  * @author Everton
@@ -18,13 +23,21 @@ public class SessionListener implements HttpSessionListener {
 	private static int totalActiveSessions;
 	
     public void sessionCreated(HttpSessionEvent event) {        
-         System.out.println("Sessão criada " + event.getSession().getId());
+         System.out.println("Sessão criada");
          System.out.println("Sessões ativas: "+(++totalActiveSessions));
     }
  
-    public void sessionDestroyed(HttpSessionEvent event) {     
+    @SuppressWarnings("unchecked")
+	public void sessionDestroyed(HttpSessionEvent event) {     
          String ultimoAcesso = (new SimpleDateFormat("dd/MM/yyyy HH:mm:ss")).format(new Date(event.getSession().getLastAccessedTime()));
-         System.out.println("Sessão expirada "+event.getSession().getId()+". Ultimo Acesso = "+ultimoAcesso);
+         Map<String, PessoaDTO> mapaUsuarios = (Map<String, PessoaDTO>) event.getSession().getAttribute(ConstantesWebFitness.LOGIN_USER.getValor());
+         String nomeUsuario = "";
+         if(mapaUsuarios != null){
+        	 for (Entry<String, PessoaDTO> usuario : mapaUsuarios.entrySet()) {
+				nomeUsuario = usuario.getValue().getNome();
+			}
+         }
+         System.out.println("Sessão do usuario "+nomeUsuario+" expirada. Ultimo Acesso = "+ultimoAcesso);
          System.out.println("Sessões ativas: "+(--totalActiveSessions));
     }
  
